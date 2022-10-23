05d3785ce065602534a4ea9558aa7649bf60fe3b
suneel kumar <suneelsuther123@gmail.com>
2022-10-23 07:10:20 +0500

    Update index.js
    removed firebase

--- src/config/index.js
+++ src/config/index.js
@@ -5,14 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // add firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCVbMGXVykkHlfWV2_W_-eP4DkwcA5rR-w",
-  authDomain: "vagantrackerapp.firebaseapp.com",
-  databaseURL: "https://vagantrackerapp-default-rtdb.firebaseio.com",
-  projectId: "vagantrackerapp",
-  storageBucket: "vagantrackerapp.appspot.com",
-  messagingSenderId: "173257995549",
-  appId: "1:173257995549:web:3e8cdc32adac2decb3ddf4",
-  measurementId: "G-SXPT1T32FN",
+
 };
 
 // initialize firebase
